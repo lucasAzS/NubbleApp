8f36a3c2f4d815bb919c17a7fb71ca1aa1814b94
lucasaz <lucasazsantos@gmail.com>
2023-12-20 12:16:28 -0300

    text preset

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {SafeAreaView, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Text} from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Text>test</Text>
+    <SafeAreaView>
+      <Text preset="headingLarge">test</Text>
     </SafeAreaView>
   );
 }
